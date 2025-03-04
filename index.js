84d40ca95b84bff4d0a6dcb1f5c1e6adecef5c91
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-04 20:19:41 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(rollerSkate = "roller-skate") {
+    return `This Saturday, I want to ${rollerSkate}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "stupendous") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
